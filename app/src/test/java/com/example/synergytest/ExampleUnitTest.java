bde58c696c185676777c1cbe3371492bdf296ab6
Bloxorer <bloxore@gmail.com>
2023-04-01 23:12:32 +0300

    creation

--- app/src/test/java/com/example/synergytest/ExampleUnitTest.java
+++ app/src/test/java/com/example/synergytest/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.example.synergytest;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Ass ert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
